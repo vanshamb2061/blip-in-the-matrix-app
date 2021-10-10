8d122c7660499380c42efe437a213cd1f226dbd7
ankitkushawaha1000 <ankitkushawaha1000@gmail.com>
2021-10-10 23:34:05 +0530

    added genre and language selection page

--- src/sample/registrationController.java
+++ src/sample/registrationController.java
@@ -1,11 +1,15 @@
 package sample;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+import javafx.stage.StageStyle;
 
 public class registrationController {
     @FXML
@@ -13,11 +17,17 @@ public class registrationController {
     @FXML
     private Button closeButton;
     @FXML
+    private Button selectGenreButton;
+    @FXML
+    private Button selectLanguageButton;
+    @FXML
     private TextField firstNameTextField;
     @FXML
     private TextField lastNameTextField;
     @FXML
     private TextField ageTextField;
+    @FXML
+    private TextField usernameTextField;
     @FXML
     private PasswordField setPasswordField;
     @FXML
@@ -27,7 +37,21 @@ public class registrationController {
         //write backend code
         //sql injection
     }
+    public void selectGenreButtonOnAction(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("selectGenre.fxml"));
+        Stage selectGenreStage = new Stage();
+        selectGenreStage.initStyle(StageStyle.UNDECORATED);
+        selectGenreStage.setScene(new Scene(root, 280, 400));
+        selectGenreStage.show();
+    }
 
+    public void selectLanguageButtonOnAction(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("selectLanguage.fxml"));
+        Stage selectLanguageStage = new Stage();
+        selectLanguageStage.initStyle(StageStyle.UNDECORATED);
+        selectLanguageStage.setScene(new Scene(root, 230, 230));
+        selectLanguageStage.show();
+    }
     public void closeButtonOnAction(ActionEvent event){
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();

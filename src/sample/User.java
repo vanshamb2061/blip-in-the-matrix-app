bdeeaae5e6fe46ce5e1c4cc3f1dd32d58a7cfd1a
Vansh <ambashtavansh@gmail.com>
2021-10-14 12:47:46 +0530

    basic classes created

---
+++ src/sample/User.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class User {
+    String username;
+    String pwd;
+    User(){
+        username="SAMPLE";
+        pwd="Password";
+    }
+    User(String username, String pwd){
+        username=username;
+        pwd=pwd;
+    }
+}
